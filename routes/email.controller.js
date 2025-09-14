1d1011eed378dd8e5c6ef77397dae86398a5f116
Ram <ramprakashdhanush@gmail.com>
2025-09-14 20:55:13 +0530

    Response changed

--- routes/email.controller.js
+++ routes/email.controller.js
@@ -3,7 +3,7 @@ const { sendEmail } = require('../emailService.js');
 function sendMail(req, res) {
   const { subject, fullName, email, text } = req.body;
   sendEmail(subject, fullName, email, text);
-  res.status(200).send('Email send successfully');
+  res.status(200);
 }
 
 module.exports = {
